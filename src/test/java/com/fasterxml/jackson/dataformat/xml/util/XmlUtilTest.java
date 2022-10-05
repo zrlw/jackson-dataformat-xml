5c92d2b9092abe8c238567bb3021cca46118962b
zrlw <zrlw@sina.com>
2022-10-05 16:00:01 +0800

    change to parallel testing

--- src/test/java/com/fasterxml/jackson/dataformat/xml/util/XmlUtilTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/util/XmlUtilTest.java
@@ -1,13 +1,14 @@
  package com.fasterxml.jackson.dataformat.xml.util;
 
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.Assert;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.dataformat.xml.testutil.MyPacketBody;
 import com.fasterxml.jackson.dataformat.xml.testutil.MyCurrency;
 import com.fasterxml.jackson.dataformat.xml.testutil.MyEnvelope;
+import com.fasterxml.jackson.dataformat.xml.testutil.MyPacketBody;
 import com.fasterxml.jackson.dataformat.xml.testutil.MyStudent;
 
 import junit.framework.TestCase;
@@ -38,27 +39,56 @@ public class XmlUtilTest extends TestCase {
  
          TypeReference<MyEnvelope<MyStudent>> myTypeReference = new TypeReference<MyEnvelope<MyStudent>>() {};
 
-         // repeat testing.
-         for (int i = 0; i < 5; i++) {
-             String envelopeXml = XmlUtil.objectToXml(myEnvelope);
-             MyEnvelope<MyStudent> myEnvelope1 = XmlUtil.xmlToObject(envelopeXml, myTypeReference);
-             Assert.assertEquals(myEnvelope, myEnvelope1);
+         // parallel testing.
+         int cnt = 5;
+         CountDownLatch latch = new CountDownLatch(cnt * 3);
+         for (int i = 0; i < cnt; i++) {
+             new Thread( () -> {
+                 MyEnvelope<MyStudent> myEnvelope1 = null;
+                 try {
+                     String envelopeXml = XmlUtil.objectToXml(myEnvelope);
+                     myEnvelope1 = XmlUtil.xmlToObject(envelopeXml, myTypeReference);
+                     Assert.assertEquals(myEnvelope, myEnvelope1);
+                     latch.countDown();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }).start();
 
-             byte[] bytes = XmlUtil.objectToBytes(myEnvelope);
-             myEnvelope1 = XmlUtil.bytesToObject(bytes, myTypeReference);
-             Assert.assertTrue(myEnvelope.equals(myEnvelope1));
+             new Thread( () -> {
+                 MyEnvelope<MyStudent> myEnvelope1 = null;
+                 try {
+                     Map<String, Object> map = XmlUtil.objectToObjectMap(myEnvelope);
+                     myEnvelope1 = XmlUtil.objectMapToObject(map, myTypeReference);
+                     Assert.assertEquals(myEnvelope, myEnvelope1);
 
-             Map<String, Object> map = XmlUtil.objectToObjectMap(myEnvelope);
-             myEnvelope1 = XmlUtil.objectMapToObject(map, myTypeReference);
-             Assert.assertEquals(myEnvelope, myEnvelope1);
+                     String envelopeXml = XmlUtil.objectMapToXml(map);
+                     myEnvelope1 = XmlUtil.xmlToObject(envelopeXml, myTypeReference);
+                     Assert.assertEquals(myEnvelope, myEnvelope1);
 
-             envelopeXml = XmlUtil.objectMapToXml(map);
-             myEnvelope1 = XmlUtil.xmlToObject(envelopeXml, myTypeReference);
-             Assert.assertEquals(myEnvelope, myEnvelope1);
-
-             myEnvelope1 = XmlUtil.objectMapToObject(map, myTypeReference);
-             Assert.assertEquals(myEnvelope, myEnvelope1);
+                     myEnvelope1 = XmlUtil.objectMapToObject(map, myTypeReference);
+                     Assert.assertEquals(myEnvelope, myEnvelope1);
+                     latch.countDown();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }).start();
+             
+             new Thread( () -> {
+                 byte[] bytes;
+                 MyEnvelope<MyStudent> myEnvelope1 = null;
+                 try {
+                     bytes = XmlUtil.objectToBytes(myEnvelope);
+                     myEnvelope1 = XmlUtil.bytesToObject(bytes, myTypeReference);
+                     Assert.assertTrue(myEnvelope.equals(myEnvelope1));
+                     latch.countDown();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                 }
+             }).start();
          }
+         
+         latch.await();
      }
 
 }
